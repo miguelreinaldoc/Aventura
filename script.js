7577ff093de886704006d54bc7f315c9fd9897f1
miguelreinaldoc <miguel.claudino@escola.pr.gov.br>
2025-07-03 10:47:17 +0000

    aventura 1b

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach (button =>{
+avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = decodeURIComponent.querySelector('.ativo');
+        const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     })
-
 })
+
